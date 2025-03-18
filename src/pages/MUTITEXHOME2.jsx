711a9f725c03e6f73811a1f78f2be1e99a318515
ManjulTiwari22 <manjul8874@gmail.com>
2025-03-18 16:06:24 +0530

    initialize project with Vite, React, and basic structure

---
+++ src/pages/MUTITEXHOME2.jsx
@@ -0,0 +1,232 @@
+import "./MUTITEXHOME2.css";
+
+const MUTITEXHOME2 = () => {
+  return (
+    <div className="mutitex-home-2">
+      <img
+        className="petroleum-gas-container-ship-o-icon"
+        alt=""
+        src="/petroleumgascontainershipoilrefinerybackgroundenergynauticaltransportation@2x.png"
+      />
+      <div className="petroleum-gas-container-ship-o" />
+      <img className="path-99-icon" alt="" src="/path-99.svg" />
+      <img className="path-97-icon" alt="" src="/path-99.svg" />
+      <img className="path-98-icon" alt="" src="/path-98.svg" />
+      <div className="ultitex-parent">
+        <b className="ultitex">ULTITEX</b>
+        <img className="group-child" alt="" src="/group-76.svg" />
+      </div>
+      <div className="mutitex-home-2-child" />
+      <div className="efficiency-meets-reliability-parent">
+        <b className="efficiency-meets-reliability">
+          Efficiency meets reliability
+        </b>
+        <div className="whether-youre-looking-container">
+          <p className="whether-youre-looking">{`Whether you’re looking for high-quality gasoline or innovative `}</p>
+          <p className="whether-youre-looking">{`solutions to power your home or business, we’ve got you `}</p>
+          <p className="whether-youre-looking">covered.</p>
+        </div>
+        <div className="years-on-market">Years on market</div>
+        <div className="big-projects">Big projects</div>
+        <div className="trusted-partners">Trusted partners</div>
+        <div className="your-energy-solutions-container">
+          <p className="whether-youre-looking">{`Your energy `}</p>
+          <p className="whether-youre-looking">solutions provider</p>
+        </div>
+        <div className="div">41+</div>
+        <div className="k">3K+</div>
+        <div className="div1">60+</div>
+        <div className="group-item" />
+        <div className="group-inner" />
+        <b className="contact-us">Contact us</b>
+        <b className="discover-more">Discover more</b>
+      </div>
+      <b className="about-us">About us</b>
+      <b className="our-services">Our services</b>
+      <b className="testmonials">Testmonials</b>
+      <div className="lorem-ipsum-is">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </div>
+      <div className="learn-about-our-container">
+        <p className="whether-youre-looking">Learn about our history,</p>
+        <p className="whether-youre-looking">mission, and values</p>
+      </div>
+      <div className="discover-the-range-container">
+        <p className="whether-youre-looking">{`Discover the range of `}</p>
+        <p className="whether-youre-looking">services we offer</p>
+      </div>
+      <div className="why-people-consider-container">
+        <p className="whether-youre-looking">{`Why people consider us `}</p>
+        <p className="whether-youre-looking">a reliable partner</p>
+      </div>
+      <div className="mutitex-home-2-item" />
+      <div className="mutitex-home-2-inner" />
+      <div className="rectangle-div" />
+      <div className="mutitex-home-2-child1" />
+      <img
+        className="infographic-template-icon"
+        alt=""
+        src="/infographictemplate@2x.png"
+      />
+      <img
+        className="steel-pipelines-cables-plant-icon"
+        alt=""
+        src="/steelpipelinescablesplant@2x.png"
+      />
+      <img
+        className="engineer-technician-designing-icon"
+        alt=""
+        src="/engineertechniciandesigningdrawingsmechanicalpartsengineeringenginemanufacturingfactory@2x.png"
+      />
+      <img
+        className="centre-georges-pompidou-paris-icon"
+        alt=""
+        src="/centregeorgespompidouparisfrance@2x.png"
+      />
+      <img
+        className="workers-control-room-icon"
+        alt=""
+        src="/workerscontrolroom@2x.png"
+      />
+      <img
+        className="electrician-tools-electrical-e-icon"
+        alt=""
+        src="/electriciantoolselectricalequipmentwiringdiagram@2x.png"
+      />
+      <b className="discover-more1">Discover more</b>
+      <img
+        className="iuthg4-787x1024-icon"
+        alt=""
+        src="/4iuthg4787x1024@2x.png"
+      />
+      <div className="repeat-grid-3">
+        <div className="div2">
+          <div className="company">COMPANY</div>
+        </div>
+        <div className="div3">
+          <div className="company">COMPANY</div>
+        </div>
+        <div className="div4">
+          <div className="company">COMPANY</div>
+        </div>
+        <div className="div5">
+          <div className="company">COMPANY</div>
+        </div>
+        <div className="div6">
+          <div className="company">COMPANY</div>
+        </div>
+        <div className="div7">
+          <div className="company">COMPANY</div>
+        </div>
+        <div className="div8">
+          <div className="company">COMPANY</div>
+        </div>
+      </div>
+      <img
+        className="i24uthg4-1024x788-icon"
+        alt=""
+        src="/i24uthg41024x788@2x.png"
+      />
+      <div className="to-drive-innovation-container">
+        <p className="whether-youre-looking">{`To drive innovation and progress in the `}</p>
+        <p className="whether-youre-looking">energy industry</p>
+      </div>
+      <div className="as-a-leading-container">
+        <p className="whether-youre-looking">{`As a leading player in the oil and gas industry, `}</p>
+        <p className="whether-youre-looking">{`we're committed to delivering the energy `}</p>
+        <p className="whether-youre-looking">
+          solutions that keep the world moving
+        </p>
+      </div>
+      <div className="div9">10%</div>
+      <div className="div10">20%</div>
+      <div className="line-div" />
+      <div className="mutitex-home-2-child2" />
+      <div className="lower-prices-than-container">
+        <p className="whether-youre-looking">{`lower prices than competitors . Our company is able to keep the price of oil and gas at the `}</p>
+        <p className="whether-youre-looking">affirdable level.</p>
+      </div>
+      <div className="reducing-greenhouse-gas-container">
+        <p className="whether-youre-looking">{`reducing greenhouse gas emissions per year. We're committed to minimizing our `}</p>
+        <p className="whether-youre-looking">
+          environmental impact and promoting social responsibility
+        </p>
+      </div>
+      <div className="reducing-greenhouse-gas-container1">
+        <p className="whether-youre-looking">{`reducing greenhouse gas emissions per year. We're committed to minimizing our `}</p>
+        <p className="whether-youre-looking">
+          environmental impact and promoting social responsibility
+        </p>
+      </div>
+      <div className="we-expand-their">
+        We expand their refinery facilities to increase our production capacity.
+      </div>
+      <div className="we-invest-in">
+        We invest in new pipeline infrastructure to transport oil and gas more
+        efficiently .
+      </div>
+      <div className="we-invest-in1">
+        We invest in technologies that reduce emissions and waste.
+      </div>
+      <div className="we-explore-and">
+        We explore and drill in new areas to discover and extract new oil and
+        gas reserves.
+      </div>
+      <b className="renewable-energy-development">
+        Renewable energy development
+      </b>
+      <b className="refinery-expansion">Refinery expansion</b>
+      <b className="transport-and-logistics">Transport and logistics</b>
+      <b className="environmental-remedia">Environmental remedia</b>
+      <b className="exploration-and-drilling">Exploration and drilling</b>
+      <img className="image-5-icon" alt="" src="/image-5@2x.png" />
+      <img className="image-6-icon" alt="" src="/image-5@2x.png" />
+      <img className="ellipse-icon" alt="" src="/ellipse-6.svg" />
+      <img className="mutitex-home-2-child3" alt="" src="/ellipse-6.svg" />
+      <img className="polygon-icon" alt="" src="/polygon-1.svg" />
+      <img className="mutitex-home-2-child4" alt="" src="/polygon-1.svg" />
+      <div className="process-design">Process Design</div>
+      <div className="piping-design">Piping Design</div>
+      <div className="mechanical-design">Mechanical Design​</div>
+      <div className="structural-design">Structural Design</div>
+      <div className="control-instrumentation-container">
+        <p className="whether-youre-looking">{`Control & `}</p>
+        <p className="whether-youre-looking">Instrumentation​</p>
+      </div>
+      <div className="electrical-automation-container">
+        <p className="whether-youre-looking">{`Electrical & `}</p>
+        <p className="whether-youre-looking">Automation</p>
+      </div>
+      <img className="ori3jg3-icon" alt="" src="/ori3jg3@2x.png" />
+      <img className="o4tjh4-icon" alt="" src="/o4tjh4@2x.png" />
+      <img
+        className="k5j4nt-1536x1025-icon"
+        alt=""
+        src="/k5j4nt1536x1025@2x.png"
+      />
+      <img className="o4tijg-icon" alt="" src="/o4tijg@2x.png" />
+      <img className="o42ijtg-icon" alt="" src="/o42ijtg@2x.png" />
+      <img className="mask-group-1" alt="" src="/mask-group-1@2x.png" />
+      <img className="mask-group-2" alt="" src="/mask-group-1@2x.png" />
+      <img className="mutitex-home-2-child5" alt="" src="/ellipse-8.svg" />
+      <img className="mutitex-home-2-child6" alt="" src="/ellipse-9.svg" />
+      <img className="mutitex-home-2-child7" alt="" src="/ellipse-8.svg" />
+      <div className="from-the-well-container">
+        <p className="whether-youre-looking">{`From the well to the pump, `}</p>
+        <p className="whether-youre-looking">we've got you covered</p>
+      </div>
+      <div className="mutitex-home-2-child8" />
+      <b className="contact-us1">Contact us</b>
+    </div>
+  );
+};
+
+export default MUTITEXHOME2;
